e583c0696e9c4066c1857f3124d83bc8bc527203
shadow <724132485@qq.com>
2021-04-02 15:52:14 +0800

    chore(umirc.ts): 更新打包配置

--- .umirc.ts
+++ .umirc.ts
@@ -2,15 +2,18 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'KJJ-FE-COMMON',
-  favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   // logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   navs: [
     {
       title: 'github',
-      path: 'https://github.com/stack-wuh'
-    }
+      path: 'https://github.com/stack-wuh',
+    },
   ],
-  description: "更简单, 更快速!",
+  description: '更简单, 更快速!',
+  base: '/doc-common/',
+  publicPath: '/doc-common/',
   // more config: https://d.umijs.org/config
 });
